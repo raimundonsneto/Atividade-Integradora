88f3289dccc570ec96f20f86c8445e1c8271b740
raimundonsneto <119816169+raimundonsneto@users.noreply.github.com>
2022-12-05 11:40:58 -0300

    Update Main.java
    Inserindo a parte final do código

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 /*
  * Fazer um programa que leia três valores com ponto flutuante de dupla
  * precisão: A, B e C. Em seguida, calcule e mostre: a) a área do triângulo
@@ -6,13 +7,13 @@
  * d) a área do quadrado que tem lado B. e) a área do retângulo que tem lados A
  * e B.
  */
+
 import java.util.Locale;
 import java.util.Scanner;
 
-
 public class Main {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
@@ -22,16 +23,20 @@ public class Main {
 		A = sc.nextDouble();
 		B = sc.nextDouble();
 		C = sc.nextDouble();
-		
+
 		triangulo = A * C / 2.0;
 		circulo = 3.14159 * C * C;
 		trapezio = (A + B) / 2.0 * C;
 		quadrado = B * B;
 		retangulo = A * B;
 
-		
-		sc.close();
+		System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		System.out.printf("CIRCULO: %.3f%n", circulo);
+		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		System.out.printf("QUADRADO: %.3f%n", quadrado);
+		System.out.printf("RETANGULO: %.3f%n", retangulo);
 
+		sc.close();
 
 	}
 
